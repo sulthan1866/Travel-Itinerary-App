58ae5ee453601a975b9d12f93b6fc6c2c3e30cd0
sulthan1866 <mohammed.sulthan.2972004@gmail.com>
2025-04-27 22:11:14 +0530

    rollback vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
-  server: {
-    historyApiFallback: true,
-  },
 });
